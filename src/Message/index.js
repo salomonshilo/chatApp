ef575faefea0d6816bb1d11eaf41cdf44180669d
salomonshilo <shilomavonga@mail.com>
2022-10-18 15:57:00 +0100

    integration frontend search bar

--- src/Message/index.js
+++ src/Message/index.js
@@ -7,14 +7,22 @@ function Chat() {
     <div className='container_chat'>
 
             <div className='container_user'> 
-            
+                    <div className='image'> <img src='https://img.freepik.com/premium-vector/person-avatar-design_24877-38137.jpg?w=2000'/>  </div>
+                    <div className='message_logo'> <i class="uil uil-comment-message message"></i> </div>
+                    <div className='logout_logo'> <i class="uil uil-signout"></i></div>
             
             </div>
+
             <div className='container_recent_message'> 
                  <div className='search_bar'>
+                    
+                    <i class="uil uil-search"></i>
                     <p> Search</p>
+                    <i class="uil uil-ellipsis-v more_logo"></i>
                  </div>
-                 <div className='recent_message'></div>
+                 <div className='recent_message'>
+
+                 </div>
             
             </div>
             <div className='container_conversation'> 
